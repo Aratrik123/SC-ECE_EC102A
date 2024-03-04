5a508d005cb27c2e8072a2b7843912383796f0a7
sayan044 <sayan.mukherjee.work044@gmail.com>
2024-03-04 13:57:12 +0530

    Commit Message

--- Shop-Topia/src/Components/Navbar.jsx
+++ Shop-Topia/src/Components/Navbar.jsx
@@ -14,7 +14,7 @@ export const Navbar = () => {
                     <li>Categories</li>
                     <li>Featured</li>
                     <li>About Us</li>
-                    <li style={{ color: '#84A59D' }}>+91-8955XXXXXX</li>
+                    <li style={{ color: '#84A59D' }}>Contact</li>
                 </ul>
             </div>
             <div className="accounts">
